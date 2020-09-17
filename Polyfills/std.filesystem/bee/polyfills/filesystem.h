713f5b39cf4e6bd29aaa48d69172a050a99bddc1
SB <leslieleigh@outlook.com>
2020-09-17 13:39:35 +0800

    Use C++ 20 for CLI

--- Polyfills/std.filesystem/bee/polyfills/filesystem.h
+++ Polyfills/std.filesystem/bee/polyfills/filesystem.h
@@ -3,12 +3,13 @@
 
 #ifdef BEE_POLYFILLS_STD_FILESYSTEM
 #include <ghc-filesystem/filesystem.hpp>
+#include <string_view>
 namespace bee::filesystem {
 using namespace ghc::filesystem;
-}
+} // namespace bee::filesystem
 #else
 #include <filesystem>
 namespace bee::filesystem {
 using namespace std::filesystem;
-}
+} // namespace bee::filesystem
 #endif
