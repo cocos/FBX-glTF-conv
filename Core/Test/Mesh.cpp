b3cca5e246c9aa20e74aaa1568c9e49e420faf9d
AFeiYA <386665678@qq.com>
2023-01-11 16:45:15 +0800

    Create Triangles.

--- Core/Test/Mesh.cpp
+++ Core/Test/Mesh.cpp
@@ -18,36 +18,37 @@ void createFbxGrid(const char *path_, std::uint32_t N) {
   const auto gridNode = fbxsdk::FbxNode::Create(fbxManager, "Gird");
   const auto grid = fbxsdk::FbxMesh::Create(fbxManager, "Grid");
 
-    FbxVector4 *vertices = new FbxVector4[N * N];
-    for (int i = 0; i < N; i++) {
+     std::vector<FbxVector4> vertices;
+     vertices.reserve(N * N);
+      for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-          vertices[i * N + j] = FbxVector4((float)i, 0.0, (float)j);
+          vertices.push_back(FbxVector4((float)i, 0.0, (float)j));
         }
-    }
-    // Set the vertices of the grid
-    grid->InitControlPoints(N * N);
-    for (int i = 0; i < N * N; i++) {
-    grid->SetControlPointAt(vertices[i], i);
-    }
-    // Define the faces of the grid
-    int *indices = new int[(N - 1) * (N - 1) * 4];
-    int index = 0;
-    for (int i = 0; i < N - 1; i++) {
+      }
+      // Set the vertices of the grid
+      grid->InitControlPoints(N * N);
+      for (int i = 0; i < N * N; i++) {
+        grid->SetControlPointAt(vertices[i], i);
+      }
+      // Define the faces of the grid
+      std::vector<int> indices;
+      indices.reserve((N - 1) * (N - 1) * 4);
+      for (int i = 0; i < N - 1; i++) {
         for (int j = 0; j < N - 1; j++) {
-          indices[index++] = i * N + j;
-          indices[index++] = i * N + j + 1;
-          indices[index++] = (i + 1) * N + j + 1;
-          indices[index++] = (i + 1) * N + j;
+          indices.push_back(i * N + j);
+          indices.push_back(i * N + j + 1);
+          indices.push_back((i + 1) * N + j + 1);
+          indices.push_back((i + 1) * N + j);
         }
-    }
-    for (int i = 0; i < (N - 1) * (N - 1); i++) {
+      }
+      for (int i = 0; i < (N - 1) * (N - 1); i++) {
         grid->BeginPolygon(-1, -1, false);
         grid->AddPolygon(indices[i * 4 + 0]);
         grid->AddPolygon(indices[i * 4 + 1]);
         grid->AddPolygon(indices[i * 4 + 2]);
         grid->AddPolygon(indices[i * 4 + 3]);
         grid->EndPolygon();
-    }
+      }
     gridNode->SetNodeAttribute(grid);
     // Add the grid node to the scene
     fbxScene->GetRootNode()->AddChild(gridNode);
@@ -61,64 +62,63 @@ void createFbxGrid(const char *path_, std::uint32_t N) {
         exporter->Initialize(fbxFileName, -1, fbxManager->GetIOSettings());
     CHECK_EQ(success, true);
     const auto exportSuccess = exporter->Export(fbxScene);
-    //std::printf("Error returned: %s\n\n", exporter->GetStatus().GetErrorString());
     CHECK_EQ(exportSuccess, true);
-
     exporter->Destroy();
-    //fbxScene->Destroy(true);
     fbxManager->Destroy();
 }
-FbxNode* createGrid(FbxManager *pSdkManager, FbxScene *pScene, int rows, int cols) {
 
-    // Create a new mesh
-    FbxMesh *pMesh = FbxMesh::Create(pSdkManager, "Grid");
+// N : total triangles
+void createTriangles(const char *path_, int N, int M = 200) {
+    // Initialize the FBX SDK
+    FbxManager *manager = FbxManager::Create();
 
-    // Initialize the control points
-    int pointCount = rows * cols;
-    pMesh->InitControlPoints(pointCount);
-    FbxVector4 *controlPoints = pMesh->GetControlPoints();
+    // Create an FBX scene
+    FbxScene *scene = FbxScene::Create(manager, "MyScene");
 
-    // Create the vertices
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-          controlPoints[i * cols + j].Set(j, 0, i);
-        }
+    // Create a Node
+    FbxNode *trianglesNode = FbxNode::Create(scene, "MyTriangles");
+
+    // Create a mesh for the triangles
+    FbxMesh *triangleMesh = FbxMesh::Create(scene, "MyTrianglesMesh");
+    trianglesNode->AddNodeAttribute(triangleMesh);
+
+    // Create control points for the triangle mesh
+    int controlPointsCount = 3 * N;
+    triangleMesh->InitControlPoints(controlPointsCount);
+
+   // Set the control point positions
+    FbxVector4 *controlPoints = triangleMesh->GetControlPoints();
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        double x = i % M * 1.1 ;
+        double y = i / M * 1.1;
+        double z = 0;
+        controlPoints[count++] = FbxVector4(x, y, z);
+        controlPoints[count++] = FbxVector4(x+1 ,y, z);
+        controlPoints[count++] = FbxVector4(x+1 ,y+1, z);        
     }
 
-    // Create the polygons
-    for (int i = 0; i < rows - 1; i++) {
-        for (int j = 0; j < cols - 1; j++) {
-          pMesh->BeginPolygon();
-          pMesh->AddPolygon(i * cols + j);
-          pMesh->AddPolygon((i + 1) * cols + j);
-          pMesh->AddPolygon((i + 1) * cols + j + 1);
-          pMesh->EndPolygon();
-
-          pMesh->BeginPolygon();
-          pMesh->AddPolygon(i * cols + j);
-          pMesh->AddPolygon((i + 1) * cols + j + 1);
-          pMesh->AddPolygon(i * cols + j + 1);
-          pMesh->EndPolygon();
-        }
+    // Create triangles
+    int triangleCount = N;
+    for (int i = 0; i < triangleCount; i++) {
+        triangleMesh->BeginPolygon();
+        triangleMesh->AddPolygon(i * 3);
+        triangleMesh->AddPolygon(i * 3 + 1);
+        triangleMesh->AddPolygon(i * 3 + 2);
+        triangleMesh->EndPolygon();
     }
-
-    // Create a node to contain the mesh
-    FbxNode *pNode = FbxNode::Create(pScene, "GridNode");
-    pNode->SetNodeAttribute(pMesh);
-    pScene->GetRootNode()->AddChild(pNode);
-
-    // Create and assign a material to the mesh
-    FbxSurfacePhong *pMaterial =
-        FbxSurfacePhong::Create(pSdkManager, "Material");
-    pNode->AddMaterial(pMaterial);
-
-    return pNode;
+    scene->GetRootNode()->AddChild(trianglesNode);
+    FbxExporter *exporter = FbxExporter::Create(manager, "MyExporter");
+    exporter->Initialize(path_, -1, manager->GetIOSettings());
+    exporter->Export(scene);
+    exporter->Destroy();
+    manager->Destroy();
 }
 void testIndexUnit(const char *path_,
                    std::uint32_t vertex_count_,
                    fx::gltf::Accessor::ComponentType component_type_) {
   const auto fbxFileName = path_;
-  createFbxGrid(fbxFileName, (uint32_t)std::sqrt(vertex_count_));
+  createTriangles(fbxFileName, vertex_count_/3 );
   bee::ConvertOptions convertOptions;
   auto glTF = bee::_convert_test(reinterpret_cast<const char8_t *>(fbxFileName),
                                  convertOptions);
@@ -133,12 +133,11 @@ void testIndexUnit(const char *path_,
 
 TEST_CASE("Mesh") {
   SUBCASE("Index unit") {
-    // 生成65535以下数量的顶点的FBX
-    // 调用 API 转换乘 gltf
-    // 再去判断
-    testIndexUnit("Grid_256A.fbx", 256 * 256  ,
-                   fx::gltf::Accessor::ComponentType::UnsignedInt);
-    testIndexUnit("Grid_255A.fbx", 255 * 255  ,
+    //test 65538 index
+     testIndexUnit("T_65538.fbx", 65538  ,
+                   fx::gltf::Accessor::ComponentType::UnsignedInt);    
+    // test 65535 index
+    testIndexUnit("T_65535.fbx", 65535 ,
                   fx::gltf::Accessor::ComponentType::UnsignedShort);
   }
 }
