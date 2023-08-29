100004aae6e0d4a835dc08d4518320ff8bda5428
shrinktofit <leslieleigh@outlook.com>
2023-08-29 17:58:12 +0800

    Fix macOS Build

--- Core/Source/bee/Convert/SceneConverter.cpp
+++ Core/Source/bee/Convert/SceneConverter.cpp
@@ -384,7 +384,7 @@ void SceneConverter::_convertNode(fbxsdk::FbxNode &fbx_node_) {
       if (splitted.first != splitted.second) {
         std::ranges::copy(
             std::ranges::subrange(splitted.first, splitted.second) |
-                std::views::values,
+                std::views::transform([](auto kv_) { return std::get<1>(kv_); }),
             std::back_inserter(splittedMeshes));
       } else {
         splittedMeshes.push_back(mesh);
