d4e9306e3ccff1e0ac268e6db78d7959c31b9224
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2023-08-30 17:33:48 +0800

    Fix version printing (#112)

--- Cli/Version.h
+++ Cli/Version.h
@@ -3,13 +3,5 @@
 #include <string>
 
 namespace beecli {
-#ifndef FBX_GLTF_CONV_CLI_VERSION
-#  define FBX_GLTF_CONV_CLI_VERSION_STRING ""
-#else
-#  define TO_FBX_GLTF_CONV_CLI_VERSION_STRING(x) #x
-#  define TO_FBX_GLTF_CONV_CLI_VERSION_STRING2(x) TO_FBX_GLTF_CONV_CLI_VERSION_STRING(x)
-#  define FBX_GLTF_CONV_CLI_VERSION_STRING TO_FBX_GLTF_CONV_CLI_VERSION_STRING2(FBX_GLTF_CONV_CLI_VERSION)
-#endif
-
-inline const std::string version_string = FBX_GLTF_CONV_CLI_VERSION_STRING;
+extern const std::string version_string;
 } // namespace beecli
